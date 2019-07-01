d73d8ed7e30356efac41706b5aa23ecdf8dbd72a
neilswainston <neil.swainston@manchester.ac.uk>
2019-07-01 21:28:07 +0100

    Added reaction balancing.

--- liv/ergothioneine/pathway.py
+++ liv/ergothioneine/pathway.py
@@ -19,13 +19,13 @@ def update_model(model):
     add_met(model, 'herncystsulfox_c', 'hercynylcysteine sulfoxide',
             'C12H20N4O5S', 'c')
     add_met(model, 'sulfhercyn_c', '2-sulfenohercynine', 'C9H15N3O3S', 'c')
-    add_met(model, 'ergothioneine_c', 'ergothioneine', 'C9H16N3O2S', 'c')
+    add_met(model, 'ergothioneine_c', 'ergothioneine', 'C9H15N3O2S', 'c')
 
-    # L-histidine + 3 S-adenosyl-L-methionine --> hercynine +
-    # S-adenosyl-L-homocysteine
+    # L-histidine + 3 S-adenosyl-L-methionine -->
+    # hercynine + S-adenosyl-L-homocysteine + 3 H+
     reac_str = '''
         s_1006__91__c__93__ + 3 s_1416__91__c__93__ -->
-        hercynine_c + s_1413__91__c__93__
+        hercynine_c + 3 s_1413__91__c__93__ + 3 s_0794__91__c__93__
         '''
     add_reaction(model, 'Egt1a', 'Egt1a', reac_str, 'Egt1')
 
@@ -44,10 +44,10 @@ def update_model(model):
         '''
     add_reaction(model, 'Egt2', 'Egt2', reac_str, 'Egt2')
 
-    # 2-sulfenohercynine + electron donor (NADH) ->
+    # 2-sulfenohercynine + electron donor (NADH) + H+ ->
     # ergothioneine + electron_acceptor (NAD) + H20
     reac_str = '''
-        sulfhercyn_c + s_1203__91__c__93__ -->
+        sulfhercyn_c + s_1203__91__c__93__ + s_0794__91__c__93__ -->
         ergothioneine_c + s_1198__91__c__93__ + s_0803__91__c__93__
         '''
     add_reaction(model, 'ergothioneine_synthesis', 'ergothioneine synthesis',
@@ -84,6 +84,12 @@ def add_reaction(model, reaction_id, name, reac_str,
         reaction.gene_reaction_rule = gene_reaction_rule
 
     reaction.subsystem = subsystem
+
+    balance = reaction.check_mass_balance()
+
+    if balance:
+        raise ValueError(balance)
+
     return reaction
 
 
